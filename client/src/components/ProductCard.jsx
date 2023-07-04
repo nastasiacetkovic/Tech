78460e4cb66267621e0eeea0e1ad7ed383dfeffe
nastasiacetkovic <101594504+nastasiacetkovic@users.noreply.github.com>
2023-07-04 08:10:44 -0400

    redux setup and products fetched from api

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -18,7 +18,7 @@ import { Link as ReactLink } from 'react-router-dom';
 import { StarIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
 
-const Rating = ({ rating, numReviews }) => {
+const Rating = ({ rating, numberOfReviews }) => {
   const { iconSize, setIconSize } = useState('14px');
   return (
     <Flex>
@@ -30,7 +30,7 @@ const Rating = ({ rating, numReviews }) => {
         <StarIcon size={iconSize} w='14px' color={rating >= 5 ? 'orange.500' : 'gray.200'} />
       </HStack>
       <Text fontSize='md' fontWeight='bold' ml='4px'></Text>
-      {`${numReviews} ${numReviews === 1 ? 'Review' : 'Reviews'}`}
+      {`${numberOfReviews} ${numberOfReviews === 1 ? 'Review' : 'Reviews'}`}
     </Flex>
   );
 };
@@ -46,7 +46,7 @@ const ProductCard = ({ product }) => {
       rounded='lg'
       shadow='lg'
       position='relative'>
-      {product.isNew && <Circle size='10px' position='absolute' top={2} right={2} bg='green.300' />}
+      {product.productIsNew && <Circle size='10px' position='absolute' top={2} right={2} bg='green.300' />}
       {product.stock <= 0 && <Circle size='10px' position='absolute' top={2} right={2} bg='red.200' />}
       <Image p={4} src={product.image} alt={product.name} roundedTop='lg' />
       <Box flex='1' maxH='5' alignItems='baseline'>
@@ -55,7 +55,7 @@ const ProductCard = ({ product }) => {
             Sold out
           </Badge>
         )}
-        {product.isNew <= 0 && (
+        {product.productIsNew <= 0 && (
           <Badge rounded='full' px='2' fontSize='0.8em' colorScheme='green'>
             New
           </Badge>
@@ -69,14 +69,14 @@ const ProductCard = ({ product }) => {
         </Link>
       </Flex>
       <Flex justifyContent='space-between' alignContent='center' py='2'>
-        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <Rating rating={product.rating} numberOfReviews={product.numberOfReviews} />
       </Flex>
       <Flex justify='space-between'>
         <Box fontSize='2xl' color={useColorModeValue('gray.800', 'white')}>
           <Box as='span' color={'gray.600'} fontSize='lg'>
             $
           </Box>
-          {product.price.toFixed(2)}
+          {Number(product.price).toFixed(2)}
         </Box>
         <Tooltip label='Add to cart' bg='white' placement='top' color='gray.800' fontSize='1.2em'>
           <Button variant='ghost' display='flex' disabled={product.stock <= 0}>
