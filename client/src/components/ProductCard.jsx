7e38edb1f92c068fa28ef4aeb1542263a3e463e3
nastasiacetkovic <101594504+nastasiacetkovic@users.noreply.github.com>
2023-07-05 06:19:15 -0400

    add cart to item and remove

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -12,11 +12,14 @@ import {
   Link,
   HStack,
   Text,
+  useToast,
 } from '@chakra-ui/react';
 import { FiShoppingCart } from 'react-icons/fi';
 import { Link as ReactLink } from 'react-router-dom';
 import { StarIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCartItem } from '../redux/actions/cartActions';
 
 const Rating = ({ rating, numberOfReviews }) => {
   const { iconSize, setIconSize } = useState('14px');
@@ -29,12 +32,32 @@ const Rating = ({ rating, numberOfReviews }) => {
         <StarIcon size={iconSize} w='14px' color={rating >= 4 ? 'orange.500' : 'gray.200'} />
         <StarIcon size={iconSize} w='14px' color={rating >= 5 ? 'orange.500' : 'gray.200'} />
       </HStack>
-      <Text fontSize='md' fontWeight='bold' ml='4px'></Text>
-      {`${numberOfReviews} ${numberOfReviews === 1 ? 'Review' : 'Reviews'}`}
+      <Text fontSize='md' fontWeight='bold' ml='4px'>
+        {`${numberOfReviews} ${numberOfReviews === 1 ? 'Review' : 'Reviews'}`}
+      </Text>
     </Flex>
   );
 };
+
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const toast = useToast();
+  const cartInfo = useSelector((state) => state.cart);
+  const { cart } = cartInfo;
+
+  const addItem = (id) => {
+    if (cart.some((cartItem) => cartItem.id === id)) {
+      toast({
+        description: 'This item is already in your cart. Go to your cart to change the amount.',
+        status: 'error',
+        isClosable: true,
+      });
+    } else {
+      dispatch(addCartItem(id, 1));
+      toast({ description: 'Item has been added.', status: 'success', isClosable: true });
+    }
+  };
+
   return (
     <Stack
       p='2'
@@ -49,20 +72,21 @@ const ProductCard = ({ product }) => {
       {product.productIsNew && <Circle size='10px' position='absolute' top={2} right={2} bg='green.300' />}
       {product.stock <= 0 && <Circle size='10px' position='absolute' top={2} right={2} bg='red.200' />}
       <Image p={4} src={product.image} alt={product.name} roundedTop='lg' />
+
       <Box flex='1' maxH='5' alignItems='baseline'>
         {product.stock <= 0 && (
           <Badge rounded='full' px='2' fontSize='0.8em' colorScheme='red'>
             Sold out
           </Badge>
         )}
-        {product.productIsNew <= 0 && (
+        {product.productIsNew && (
           <Badge rounded='full' px='2' fontSize='0.8em' colorScheme='green'>
             New
           </Badge>
         )}
       </Box>
       <Flex mt='1' justifyContent='space-between' alignContent='center'>
-        <Link as={ReactLink} to={`/product${product._id}`} pt='2' cursor='pointer'>
+        <Link as={ReactLink} to={`/product/${product._id}`} pt='2' cursor='pointer'>
           <Box fontSize='2xl' fontWeight='semibold' as='h4' lineHeight='tight'>
             {product.name}
           </Box>
@@ -78,9 +102,9 @@ const ProductCard = ({ product }) => {
           </Box>
           {Number(product.price).toFixed(2)}
         </Box>
-        <Tooltip label='Add to cart' bg='white' placement='top' color='gray.800' fontSize='1.2em'>
-          <Button variant='ghost' display='flex' disabled={product.stock <= 0}>
-            <Icon as={FiShoppingCart} h={7} w={7} alignSelf='center'></Icon>
+        <Tooltip label='Add to cart' bg='white' placement={'top'} color={'gray.800'} fontSize={'1.2em'}>
+          <Button variant='ghost' display={'flex'} isDisabled={product.stock <= 0} onClick={() => addItem(product._id)}>
+            <Icon as={FiShoppingCart} h={7} w={7} alignSelf={'center'} />
           </Button>
         </Tooltip>
       </Flex>
